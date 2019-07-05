194a826c568deb0b0fd562ced26031852c42f145
dimmxd <31119634+dimmxd@users.noreply.github.com>
2019-07-05 16:11:12 +0800

    Add files via upload

---
+++ Gabarda_Davecyth_Booking/otherpage.js
@@ -0,0 +1,15 @@
+
+document.getElementById("Search").onclick=function(){
+  location.href="WhenSearchClick.html"};
+
+
+
+  document.getElementById("Book flight").onclick=function(){
+    location.href="Details.html"};
+  
+    document.getElementById("Edit").onclick=function(){
+      location.href="Account.html"};
+ 
+  
+      document.getElementById("savechanges").onclick=function(){
+        location.href="Account.html"};
